a1b05ac84d1362ebf226320c6cd27fdef3396a9a
Harshat-Munnangi <munnangi.harshat@gmail.com>
2022-09-04 11:18:03 +0200

    initial commit

---
+++ src/main/kotlin/com/employee/restApi/RestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.employee.restApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApiApplication>(*args)
+}
